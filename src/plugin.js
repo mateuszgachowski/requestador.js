8b5089db94ffd91b3fd2376c43ec4ba4acc8d74f
Mateusz Gachowski <mateusz.gachowski@gmail.com>
2016-09-25 02:23:29 +0200

    Fire function on assigned object of request instead of the main one

--- src/plugin.js
+++ src/plugin.js
@@ -4,8 +4,7 @@ import timeoutError from './timeout';
 
 export default function (request) {
     this.request = request;
-
-    request._timeoutError = timeoutError.bind(this);
+    this.request._timeoutError = timeoutError.bind(this);
 
     return request;
 }
